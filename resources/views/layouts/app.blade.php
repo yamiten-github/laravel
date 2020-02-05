36e109153120391155a498046fd7e3c4654fd5b5
yamiten-github <yaminotengu0310@gmail.com>
2020-02-05 15:54:23 +0900

    first commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Laravel Quickstart - Basic</title>
+
+        <!-- CSSとJavaScript -->
+    </head>
+
+    <body>
+        <div class="container">
+            <nav class="navbar navbar-default">
+                <!-- ナビバーの内容 -->
+            </nav>
+        </div>
+
+        @yield('content')
+    </body>
+</html>
+
